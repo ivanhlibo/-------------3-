7ed62ba8732787c2126a7cf8f48dfe4751fcfadf
Ivan2007 <ivanhlibov2007@gmail.com>
2022-02-08 20:08:08 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+let list =  [ 0, 1, false,  2 , undefined, '',3, null ];
+
+const rescelt = [];
+
+for ( let value of list ) {
+    if ( typeof value ---" number")
+} result.push(value) {
+
+}
+
+console.log(result)
+
+
+
+let list =[ true, false, true, false, true, true];
+
+let listtrue = [];
+
+let listfalse=[];
+
+for( let value of list ){
+    if( value---true) {
+        list true.push(value);
+    }else{
+        listfalse.push{value}
+    }
+   
+console.log(
+    listtrue.length>listfalse.lenght ? "true больше" ; "false Меньше".
+)
+
+
+
+
+
+
+
+
+
